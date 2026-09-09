5aadb8cfc9b5d473ec380b13fd577bb98fd1fcbc
McLoudConstruction <stachys@mcloudconstruction.com>
2026-09-09 02:09:07 +0000

    Clean up stray duplicate files at repo root

--- Breadcrumb.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Breadcrumb({ href, label }) {
-  return (
-    <Link href={href} className="breadcrumb-link">
-      ← {label}
-    </Link>
-  );
-}
