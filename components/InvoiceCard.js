a118ba731bb1a44992e587b9b78fd867c1cb4fe1
McLoudConstruction <stachys@mcloudconstruction.com>
2026-09-09 22:00:58 -0500

    Add files via upload

--- components/InvoiceCard.js
+++ components/InvoiceCard.js
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function InvoiceCard({ job, onSave, jobId }) {
   const router = useRouter();
