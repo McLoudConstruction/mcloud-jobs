aebcc6ba939f087528d49bdc2d8a4fc4a3b75c43
SMcLoud Repo's <stachys@mcloudconstruction.com>
2026-09-15 23:21:13 -0500

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,8 @@ import ServiceWorkerRegister from '../components/ServiceWorkerRegister';
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: 'cover',
 };
 
